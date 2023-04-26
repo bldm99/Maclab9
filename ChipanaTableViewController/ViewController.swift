e60550d55392f1e6397bb07263b2bdcf5b48d2ec
Karlo <karlo.carigga@tecsup.edu.pe>
2023-04-26 08:25:15 -0500

    Initial Commit

---
+++ ChipanaTableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChipanaTableViewController
+//
+//  Created by Mac 16 on 26/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
